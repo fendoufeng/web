2a1501b624ae60869b01ade25b4d21101318b19b
贝贝 <593104939@qq.com>
2020-03-18 23:45:15 +0800

    修改了application的配置，补充了mapper.xml的ResultMap，以及修改了备注
    Signed-off-by: 贝贝 <593104939@qq.com>

--- springbootDemo/src/main/java/com/example/web/WebApplication.java
+++ springbootDemo/src/main/java/com/example/web/WebApplication.java
@@ -5,10 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.example.web.mapper")
 public class WebApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
-
 }
